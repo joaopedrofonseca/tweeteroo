0a50413d55bc2d81613ab4b38f256394c68c00e5
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-01-06 10:21:54 -0300

    Requisitos POST

--- src/app.js
+++ src/app.js
@@ -2,8 +2,28 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 const PORT = 5000
 
 app.listen(PORT, () => { 
     console.log(`Porta do servidor: ${PORT}`)
+})
+
+let users = []
+let tweets = []
+
+app.post("/sign-up", (req,res) => {
+    let user = req.body
+    users.push(user)
+    res.send("OK")
+})
+
+app.post("/tweets", (req, res) => { 
+    let tweetWritten = req.body
+    if(!users.includes(tweetWritten.username)){
+        res.send("UNAUTHORIZED")
+    } else{
+        res.send('OK')
+    }
 })
